cbd192929eaa5da4efe93e5301a489ca9d8ca1d9
Davide Cinquini <cinquinidavide1996.dev@gmail.com>
2018-12-02 20:18:40 +0100

    loding ...

--- controller/menuDetailCtrl.js
+++ controller/menuDetailCtrl.js
@@ -1,4 +1,5 @@
-app.controller('menuDetailCtrl', function ($scope, $routeParams, $http, seoSrv) {
+app.controller('menuDetailCtrl', function (
+        $scope, $routeParams, $http, $timeout, seoSrv) {
 
     seoSrv.set('Menù prodotti', '', '');
     $http.defaults.cache = CACHE;
@@ -8,20 +9,22 @@ app.controller('menuDetailCtrl', function ($scope, $routeParams, $http, seoSrv) 
         $http.post(URL + "/menu/" + ID);
     };
 
-    $scope.get = function () {
+    $scope.get = function (wait) {
         $scope.load = true;
         $http.get(URL + "/menu/" + CategoryID)
                 .then(function (xhr) {
                     $scope.data = xhr.data;
-                    $scope.load = false;
+                    $timeout(function () {
+                        $scope.load = false;
+                    }, wait);
                 });
     };
 
     $scope.refresh = function () {
         $http.defaults.cache.remove(URL + "/menu/" + CategoryID);
-        $scope.get();
+        $scope.get(500);
     };
 
 
-    $scope.get();
+    $scope.get(0);
 });
