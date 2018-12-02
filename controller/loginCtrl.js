fa8b137128407d98a88f3a963bb2935fce5d209e
Davide Cinquini <cinquinidavide1996.dev@gmail.com>
2018-12-02 19:50:26 +0100

    login page

---
+++ controller/loginCtrl.js
@@ -0,0 +1,8 @@
+app.controller('loginCtrl', function ($scope, $location, $http, seoSrv) {
+    
+    seoSrv.set('Login', '', '');
+
+    $scope.login = function() {
+        $location.path('/menu');
+    };
+});
